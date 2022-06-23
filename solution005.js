7e9dd2355830b98fd2eee2d0c30ef8cda15b5ac9
DopeGoddess <meristins@my.palmbeachstate.edu>
2022-06-23 00:35:44 -0400

    redo

--- solution005.js
+++
@@ -1,7 +0,0 @@
-function howManyLightsabersDoYouOwn(name) {
-    return name === 'Zach' ? 18 : 0;
-}
-console.log(howManyLightsabersDoYouOwn('Zach'));
-//fundamentals reference to if/else statements
-//my goal is understand the problems 
-//practice makes improvements
