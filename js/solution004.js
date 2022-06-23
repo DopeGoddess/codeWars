66e7e3f99bea2dd1b6f040d0db975a200f18f125
DopeGoddess <meristins@my.palmbeachstate.edu>
2022-06-23 00:29:32 -0400

    solution004 completed

---
+++ js/solution004.js
@@ -0,0 +1,10 @@
+/*convert boolean values to strings "Yes
+or "No"*/
+
+function bookToWOrd(bool) {
+    if(bool = true){
+        return "Yes";
+    }
+    return "No";
+}
+
---
+++ solution005.js
@@ -0,0 +1,7 @@
+function howManyLightsabersDoYouOwn(name) {
+    return name === 'Zach' ? 18 : 0;
+}
+console.log(howManyLightsabersDoYouOwn('Zach'));
+//fundamentals reference to if/else statements
+//my goal is understand the problems 
+//practice makes improvements
