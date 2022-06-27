34b590a13cfc507a796a36c14b8ff20018f60e8b
DopeGoddess <meristins@my.palmbeachstate.edu>
2022-06-27 09:40:19 -0400

    Yes, I solve two solutions

--- js/solution007.js
+++ js/solution007.js
@@ -1,8 +1,10 @@
 // response of producing odd numbers in iterations using cotinue method
 
-for (let i = 0; i < 26; i++) {
+for (let i = 0; i < 10; i++) {
     if (i % 2 == 0) continue;
     console.log(i);
 }
+
+
 
 
---
+++ js/solution009.js
@@ -0,0 +1,7 @@
+//respone of producing even numbers in iterations 
+let i = 2;
+for (; i <= 10; i++){
+    if (i % 2 == 0){
+        console.log(i);
+    }
+}
