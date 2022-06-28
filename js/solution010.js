0d98ae596d31dcca8f0eb908a5ce55f50a14beb8
DopeGoddess <meristins@my.palmbeachstate.edu>
2022-06-28 06:38:48 -0400

    Fundamentals JS

---
+++ js/solution010.js
@@ -0,0 +1,9 @@
+// Functions & Basic language features
+// Create my first JS function helloWorld
+
+function helloWorld() {
+    let str = "Hello World!";
+    console.log(str);
+
+}
+helloWorld();
