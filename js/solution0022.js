bb7f6604fb2a9af39f1b93cc18488ee7cf9a9fd7
DopeGoddess <meristins@my.palmbeachstate.edu>
2022-07-10 09:46:40 -0400

    Finally

---
+++ js/solution0022.js
@@ -0,0 +1,17 @@
+const myArray = [];
+let i = 5;
+while (i >= 0){
+    myArray.push(i);
+    i--;
+}
+console.log(myArray);
+//this is descending of an array
+
+const myArrays = [];
+let a = 0;
+while(a < 5){
+    myArrays.push(a);
+    a++;
+}
+console.log(myArrays);
+//ascending order in an array
