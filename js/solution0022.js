719e44e391ab7b0a47bc209a59c2c98e1cf4b182
DopeGoddess <meristins@my.palmbeachstate.edu>
2022-07-13 17:02:22 -0400

    Add it Up

--- js/solution0022.js
+++ js/solution0022.js
@@ -1,17 +1,17 @@
 const myArray = [];
-let i = 5;
+let i = 5; // including 5 or interger in array
 while (i >= 0){
     myArray.push(i);
-    i--;
+    i--;// subracting a number
 }
 console.log(myArray);
-//this is descending of an array
+//this is descending of an array including 5
 
 const myArrays = [];
-let a = 0;
-while(a < 5){
+let a = 0; // include 0 
+while(a < 5){ //does not include 5 
     myArrays.push(a);
-    a++;
+    a++; // adding a number
 }
 console.log(myArrays);
 //ascending order in an array
---
+++ js/solution023.js
@@ -0,0 +1,22 @@
+//producing even 
+let i = 2;
+for (; i <= 10; i++){
+    if (i % 2 == 0)continue;{
+
+
+//producing odd 
+
+for(let i = 0; i < 10; i++){
+    if(i % 2 == 0)continue;
+}
+
+//so i know how to put it seperate
+function pickIt(arr){
+    for (var odd=[],even=[]);
+     
+     
+     
+     return [odd,even];
+   }
+
+   //working progess
