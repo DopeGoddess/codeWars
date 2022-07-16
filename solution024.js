c734ed0b178993c3ee59a7afe0762751dea769ac
Sara Meristin <89435494+DopeGoddess@users.noreply.github.com>
2022-07-15 22:10:22 -0400

    Update solution024.js

--- solution024.js
+++ solution024.js
@@ -6,5 +6,7 @@ For example, for [1, 2, 2] it should return 9 because 1^2 + 2^2 + 2^2 = 9.*/
 
 
 function squareSum(numbers){
-
+  var sum[]= sum **2;
+  if (sum == numbers){
+    return numbers;
 }
