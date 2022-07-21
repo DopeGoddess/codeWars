9a65be6a16ecf404e154f37c4b354cbb4f369325
DopeGoddess <meristins@my.palmbeachstate.edu>
2022-07-20 22:02:45 -0400

    Dubbing Time

---
+++ js/solution026.js
@@ -0,0 +1,21 @@
+//JENNY SECERT CODE
+/* Jenny has written a function that returns a greeting for a user. However, she's in love with Johnny, 
+and would like to greet him slightly different. She added a special case to her function, but she made a mistake*/
+
+//Debugging 
+//orginal problem
+
+function greet(name){
+    return "Hello, " + name + "!";
+    if(name === "Johnny")
+      return "Hello, my love!";
+  }
+
+  
+//SOLUTION
+  function greet(name){
+    if(name === "Johnny")
+    return "Hello, my love!";
+    return "Hello, " + name + "!";
+ 
+  }
