6ba7a7af010d3cec9f72ae7dbf4b394bf9659860
DopeGoddess <meristins@my.palmbeachstate.edu>
2022-07-23 00:03:33 -0400

    Math Time

---
+++ js/solution027.js
@@ -0,0 +1,11 @@
+ //Convert minutes into seconds
+
+
+function convert(minutes) {
+    return minutes * 60;
+}
+// math problems 1 minute = 60 seconds
+
+//another way to write code 
+
+const convert = (minutues) => minutes * 60;
