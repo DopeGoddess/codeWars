614a4fcf911eec416c5f99c13114fc23e7f1245e
DopeGoddess <meristins@my.palmbeachstate.edu>
2022-08-20 18:50:36 -0400

    Working Progress

---
+++ js/solution034.js
@@ -0,0 +1,11 @@
+/*Write a function which calculates the average of the numbers in a given list.
+
+Note: Empty arrays should return 0.
+
+ */
+
+function find_average(array){
+    return 0;
+}
+
+//average is calcuated by adding a group of numbers and then dividing by the count of those numbers
