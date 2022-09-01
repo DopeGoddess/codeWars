f5f6b3fe7b075f83b77ff35584660c5e45d3c466
DopeGoddess <meristins@my.palmbeachstate.edu>
2022-08-31 21:32:53 -0400

    Replacement

---
+++ js/solution038.js
@@ -0,0 +1,7 @@
+//Write function RemoveExclamationMarks which removes all exclamation marks from a given string.
+
+function removeExclamationMarks(s) {
+    return s.replace(/!/g,'');
+  }
+  //the goal is remove all the exclamation marks at once
+  //looking for replace of new string or char
